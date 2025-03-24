675252b84b673b8e109e1a0929fb32e017b60c6a
STVEEEN <cuper.fcb@gmail.com>
2025-03-23 22:20:09 -0600

    agregue rutas a app y creee carpetas de colecciones faltantes, tenemos que agregar el id de address o en el usuario o en el pedido

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,11 @@
 import express from "express"
 import employeesRoutes from "./src/routes/EmployeesRoutes.js"
 import userRoutes from "./src/routes/userRoutes.js"
+import categoriesRoutes from "./src/routes/categoriesRoutes.js"
+import productsRoutes from "./src/routes/productsRoutes.js"
+import cartRoutes from "./src/routes/cartRoutes.js"
+import orderRoutes from "./src/routes/orderRoutes.js"
+import paymentsRoutes from "./src/routes/paymentsRoutes.js"
 
 const app = express();
 
@@ -8,5 +13,11 @@ app.use(express.json());
 
 app.use("/api/employees", employeesRoutes)
 app.use("./api/user", userRoutes)
+app.use("./api/categories", categoriesRoutes)
+app.use("./api/products", productsRoutes)
+app.use("./api/cart", cartRoutes)
+app.use("./api/order", orderRoutes)
+app.use("/.api/payments", paymentsRoutes)
+
 
 export default app;
